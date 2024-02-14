8ee5b977d6ae06c61555484fd39fbc75bdcc821e
Davi Ribeiro <davi.rb@hotmail.com>
2024-02-13 22:00:45 -0300

    feat: Decrease font size

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return(
       <Card className="rounded-none border-none">
         <CardContent className="flex justify-center items-center p-5">
-          <p>© 2023 Copyright FSW Barber</p>  
+          <p className="text-xs">© 2023 Copyright <strong>FSW Barber</strong></p>  
         </CardContent>
       </Card>
   )
