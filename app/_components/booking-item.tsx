9bfe54ed413f7c6bb24dd71cd82eabc2d9d2e60b
Davi Ribeiro <davi.rb@hotmail.com>
2024-02-13 22:02:23 -0300

    feat: badge size

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -4,15 +4,15 @@ import { Badge } from "./ui/badge";
 import { Card, CardContent } from "./ui/card";
 import { ptBR } from "date-fns/locale";
 
-export default async function BookingItem() {
+export default function BookingItem() {
   
 
   return(
     <Card className="min-w-full">
       <CardContent className="grid grid-cols-[70%,30%] p-0">
         <div>
-          <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge variant={"default"} className="w-fit">Confirmado</Badge>
+          <div className="flex flex-col gap-2 py-2 pl-5">
+              <Badge variant={"default"} className="w-fit text-xs">Confirmado</Badge>
               <h2 className="text-xl font-bold">Corte de Cabelo</h2>
           
           <div className="flex gap-2 items-center">
